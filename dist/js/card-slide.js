f6e9c57c9dbbee8dea616d44b08a85cdbda318e3
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-09-21 19:21:14 -0400

    fixed card sizes as per request, trying to close card transition when no longer visible on screen... not working..

--- dist/js/card-slide.js
+++ dist/js/card-slide.js
@@ -5,23 +5,22 @@ $(document).ready(function() {
             var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            // console.log(bottom_of_element);
-            // console.log(bottom_of_window);
-
-            // console.log($(this).offset().top);
-            // console.log($(this).outerHeight());
-            // console.log($(this).outerHeight()/2);
-
-            // console.log($(window).scrollTop());
-            // console.log($(window).height());
+            console.log(bottom_of_element);
+            console.log(bottom_of_window);
 
             let cardHalf = $(this).outerHeight()/2;
+
+            let top_of_window = $(window).scrollTop();
+            let top_of_element = $(this).offset().top;
+
+            console.log(top_of_window);
+            console.log(top_of_element);
             
-            if( bottom_of_window > (bottom_of_element - cardHalf) ){
-                $('.card1').animate({'left':'75px'},1000);
-                $('.card2').animate({'right':'75px'},1000);
+            if( (bottom_of_window > (bottom_of_element - cardHalf)) && (top_of_window < top_of_element)){
+                $('.card1').animate({'left':'115px'},1000);
+                $('.card2').animate({'right':'115px'},1000);
             }
-            else if ($(window).scrollTop() > bottom_of_element){
+            else if (top_of_window > bottom_of_element-10){
                 $('.card1').animate({'left':'200px'}, 1000);
                 $('.card2').animate({'right':'200px'}, 1000);
             }
