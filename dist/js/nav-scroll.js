fc38a458cd388c2a5b902e875001b0b095a15aec
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-10-02 18:35:00 -0400

    white transparent nav bar as asked for

--- dist/js/nav-scroll.js
+++ dist/js/nav-scroll.js
@@ -14,10 +14,10 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
     document.getElementById("main-nav").style.padding = "1rem 0rem";
-    document.getElementById("nav-out").style.background = "#f6f6f6";
+    document.getElementById("nav-out").style.background = "rgb(255, 255, 255, 0.95)";
     // grey #d9d9d9
-    document.getElementById("nav-out").style.boxShadow = "0px 3px 5px #d9d9d9";
-    document.getElementById("nav-out").style.borderBottom = "10px solid #f9f9f9"
+    // document.getElementById("nav-out").style.boxShadow = "0px 3px 5px rgb(217, 217, 217, 0.9)";
+    // document.getElementById("nav-out").style.borderBottom = "10px solid #f9f9f9"
 
   } else {
     document.getElementById("main-nav").style.padding = "3rem 0rem";
