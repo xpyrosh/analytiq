d9dae0300ce2a12591802cc01512f6d497a22a11
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-10-02 20:36:45 -0400

    scroll button fixed, it was staying in DOM while invisible

--- dist/js/return-button.js
+++ dist/js/return-button.js
@@ -3,27 +3,30 @@ $(document).ready(function(){
     let visible = false;
     var bottom_of_window = $(window).scrollTop() + $(window).height();
 
+
+    $("#toTop").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        $('#toTop').fadeOut("slow");
+        return false;
+    });
+
     $(window).scroll(function(){
         let currentPosition = $(window).scrollTop() + $(window).height();
         let top_of_window = $(window).scrollTop();
 
         if ((currentPosition > prevPosition) && visible == false){
-            console.log('scrolling down');
-            $('#toTop').hide();
-            $('#toTop').fadeTo("fast", 0);
+            // console.log('scrolling down');
+            
+            $('#toTop').fadeOut("slow");
             visible = true;
         }
-        else if ((currentPosition < prevPosition) && visible == true && top_of_window > bottom_of_window){
-            console.log('scrolling up');
-            $('#toTop').show();
-            $('#toTop').fadeTo("fast", 1);
+        else if ((currentPosition < prevPosition) && visible == true && (top_of_window > bottom_of_window)){
+            // console.log('scrolling up');
+            
+            $('#toTop').fadeIn("slow");
             visible = false;
         }
-
         prevPosition = currentPosition;
     });
 });
-
-// function hideBtn(){
-//     document.getElementById('toTop').style.visibility = "hidden";
-// }
